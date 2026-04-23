e840653e948018ce79e726b99347dbf30a514aa2
EasyXdc <1330064686@qq.com>
2026-04-23 11:34:07 +0800

    refactor: remove unused ExitCode enum from error module

--- src/error.rs
+++ src/error.rs
@@ -1,18 +1,3 @@
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub enum ExitCode {
-    Success,
-    Failure,
-}
-
-impl ExitCode {
-    pub fn as_i32(self) -> i32 {
-        match self {
-            Self::Success => 0,
-            Self::Failure => 1,
-        }
-    }
-}
-
 #[derive(Debug)]
 pub enum PortError {
     CommandMissing(String),
